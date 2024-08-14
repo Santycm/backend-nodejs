69ca02b6f45cec08f961f57dd58e17c74cc3aae1
Santycm <santicmoreno@gmail.com>
2024-08-14 17:17:10 -0500

    finish add error handler

--- src/controllers/employees.controller.js
+++ src/controllers/employees.controller.js
@@ -26,66 +26,88 @@ export const getEmployee = async (req, res) => {
     console.error(error);
     return res.status(500).json({ message: "Internal Server Error" });
   }
-
-  
 };
 
 export const postEmployee = async (req, res) => {
-  const { name, salary } = req.body;
+  try {
+    const { name, salary } = req.body;
 
-  const [rows] = await pool.query(
-    "INSERT INTO employee(name, salary) VALUES(?, ?)",
-    [name, salary]
-  );
+    const [rows] = await pool.query(
+      "INSERT INTO employee(name, salary) VALUES(?, ?)",
+      [name, salary]
+    );
 
-  res.send({
-    id: rows.insertId,
-    name,
-    salary,
-  });
+    res.send({
+      id: rows.insertId,
+      name,
+      salary,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
 };
 
 export const deleteEmployee = async (req, res) => {
-  const [result] = await pool.query("DELETE FROM employee WHERE id = ?", [
-    req.params.id,
-  ]);
+  try {
+    const [result] = await pool.query("DELETE FROM employee WHERE id = ?", [
+      req.params.id,
+    ]);
 
-  if (result.affectedRows <= 0)
-    return res.status(404).json({ message: "Employee not found" });
+    if (result.affectedRows <= 0)
+      return res.status(404).json({ message: "Employee not found" });
 
-  res.status(200).json({ message: "Employee deleted" });
+    res.status(200).json({ message: "Employee deleted" });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
 };
 
 export const putEmployee = async (req, res) => {
-  const { id } = req.params;
-  const { name, salary } = req.body;
+  try {
+    const { id } = req.params;
+    const { name, salary } = req.body;
 
-  const [result] = await pool.query(
-    "UPDATE employee SET name = ?, salary = ? WHERE id = ?",
-    [name, salary, id]
-  );
+    const [result] = await pool.query(
+      "UPDATE employee SET name = ?, salary = ? WHERE id = ?",
+      [name, salary, id]
+    );
 
-  if (result.affectedRows <= 0)
-    return res.status(404).json({ message: "Employee not found" });
+    if (result.affectedRows <= 0)
+      return res.status(404).json({ message: "Employee not found" });
 
-  const [rows] = await pool.query("SELECT * FROM employee WHERE id = ?", [id]);
+    const [rows] = await pool.query("SELECT * FROM employee WHERE id = ?", [
+      id,
+    ]);
 
-  res.status(200).json({ message: "Employee updated", employee: rows[0] });
+    res.status(200).json({ message: "Employee updated", employee: rows[0] });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
 };
 
 export const patchEmployee = async (req, res) => {
-  const { id } = req.params;
-  const { name, salary } = req.body;
+  try {
+    const { id } = req.params;
+    const { name, salary } = req.body;
 
-  const [result] = await pool.query(
-    "UPDATE employee SET name = IFNULL(?, name), salary = IFNULL(?, salary) WHERE id = ?",
-    [name, salary, id]
-  );
+    const [result] = await pool.query(
+      "UPDATE employee SET name = IFNULL(?, name), salary = IFNULL(?, salary) WHERE id = ?",
+      [name, salary, id]
+    );
 
-  if (result.affectedRows <= 0)
-    return res.status(404).json({ message: "Employee not found" });
+    if (result.affectedRows <= 0)
+      return res.status(404).json({ message: "Employee not found" });
 
-  const [rows] = await pool.query("SELECT * FROM employee WHERE id = ?", [id]);
+    const [rows] = await pool.query("SELECT * FROM employee WHERE id = ?", [
+      id,
+    ]);
 
-  res.status(200).json({ message: "Employee updated", employee: rows[0] });
+    res.status(200).json({ message: "Employee updated", employee: rows[0] });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
 };
