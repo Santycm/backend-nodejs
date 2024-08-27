a40d5254644d3ef933418f1ebea5315017055ce0
Santycm <santicmoreno@gmail.com>
2024-08-27 17:16:44 -0500

    test

--- src/index.js
+++ src/index.js
@@ -2,3 +2,5 @@ import app from "./app.js";
 import {PORT} from "./config.js";
 
 app.listen(PORT);
+
+console.log(`Server running on ${location.protocol}://${location.hostname}/${location.host}/${PORT}`);
